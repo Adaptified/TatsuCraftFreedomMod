9e39691e49ac5d998699dcc1cdfa9aa6fa570e71
John J. Miller <a784b2a1@opayq.com>
2015-02-17 14:47:35 -0500

    Resolve issue #12

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_smite.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_smite.java
@@ -1,6 +1,9 @@
 package me.StevenLawson.TotalFreedomMod.Commands;
 
 import me.StevenLawson.TotalFreedomMod.TFM_Util;
+import me.StevenLawson.TotalFreedomMod.TotalFreedomMod;
+import net.minecraft.util.org.apache.commons.lang3.ArrayUtils;
+import net.minecraft.util.org.apache.commons.lang3.StringUtils;
 import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
@@ -10,13 +13,13 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 @CommandPermissions(level = AdminLevel.SUPER, source = SourceType.BOTH)
-@CommandParameters(description = "Someone being a little bitch? Smite them down...", usage = "/<command> [playername]")
+@CommandParameters(description = "Smitten a user who isn't following the rules", usage = "/<command> [playername] [reason]")
 public class Command_smite extends TFM_Command
 {
     @Override
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
-        if (args.length != 1)
+        if (args.length < 1)
         {
             return false;
         }
@@ -25,29 +28,53 @@ public class Command_smite extends TFM_Command
 
         if (player == null)
         {
-            playerMsg(TFM_Command.PLAYER_NOT_FOUND);
+            playerMsg(TotalFreedomMod.PLAYER_NOT_FOUND);
+            return true;
+        }
+        else if (args.length > 1)
+        {
+            final String reason = StringUtils.join(ArrayUtils.subarray(args, 1, args.length), " ");
+            smite(player, reason);
             return true;
         }
 
-        smite(player);
+        else
+        {
+            smite(player);
+            TFM_Util.playerMsg(sender, "You are required to use smite reasons!", ChatColor.RED);
+            if (sender instanceof Player)
+            {
+                ((Player) sender).setHealth(0d);
+            }
+        }
 
         return true;
     }
 
+    public static void smite(final Player player, final String reason)
+    {
+        TFM_Util.bcastMsg(String.format("%s has been a naughty, naughty gal.\nThey have thus been smitten!\n" + ChatColor.GOLD + " Reason: %s", player.getName(), reason), ChatColor.RED);
+        String full = String.format(ChatColor.RED + "%s has been smitten for %s", player.getName(), reason);
+        player.setGameMode(GameMode.SURVIVAL);
+        player.getInventory().clear();
+        final Location targetPos = player.getLocation();
+        final World world = player.getWorld();
+        for (int x = -1; x <= 1; x++)
+        {
+            for (int z = -1; z <= 1; z++)
+            {
+                final Location strike_pos = new Location(world, targetPos.getBlockX() + x, targetPos.getBlockY(), targetPos.getBlockZ() + z);
+                world.strikeLightning(strike_pos);
+            }
+        }
+        player.setHealth(0.0);
+    }
+
     public static void smite(final Player player)
     {
-        TFM_Util.bcastMsg(player.getName() + " has been a naughty, naughty boy.", ChatColor.RED);
-
-        //Deop
-        player.setOp(false);
-
-        //Set gamemode to survival:
+        TFM_Util.bcastMsg(player.getName() + " has been a naughty, naughty gal.\n", ChatColor.RED);
         player.setGameMode(GameMode.SURVIVAL);
-
-        //Clear inventory:
         player.getInventory().clear();
-
-        //Strike with lightning effect:
         final Location targetPos = player.getLocation();
         final World world = player.getWorld();
         for (int x = -1; x <= 1; x++)
@@ -63,3 +90,4 @@ public class Command_smite extends TFM_Command
         player.setHealth(0.0);
     }
 }
+// This command is based on FOP's smite:  https://bitbucket.org/Camzie99/freedomopmod/raw/4a7df8f581789ff647b4c31e7dbe0b20fc11babf/src/me/StevenLawson/TotalFreedomMod/Commands/Command_smite.java
